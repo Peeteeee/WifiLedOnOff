e843bb03dfa6fbed8d96c46de907478466fef433
Peeteeee <petrkucera1024@gmail.com>
2024-08-13 21:47:58 +0200

    huraaa

--- include/main.h
+++ include/main.h
@@ -33,17 +33,17 @@
 #define D5 GPIO_NUM_21
 #define D6 GPIO_NUM_22
 #define D7 GPIO_NUM_23
-
-#define tlacitko1 GPIO_NUM_12
-#define tlacitko2 GPIO_NUM_13
-#define tlacitko3 GPIO_NUM_14
+// 12, 13, 14, 15 ... JTAG
+#define tlacitko1 GPIO_NUM_25
+#define tlacitko2 GPIO_NUM_26
+#define tlacitko3 GPIO_NUM_33
 #define ledka GPIO_NUM_32
 #define FILE_PATH "/spiffs/uvodniStrana.html"
 #define FILE_PATH2 "/spiffs/vkladaniDat.html"
 #define FILE_PATH3 "/spiffs/zmenaDat.html"
 #define FILE_PATH4 "/spiffs/poleStruktur.bin"
 
-#define MAX_RUZNYCH_POSTRIKU 20
+#define MAX_RUZNYCH_POSTRIKU 50
 #define EXAMPLE_ESP_WIFI_SSID "TP-LINK_0D7A"
 #define EXAMPLE_ESP_WIFI_PASS "cabracina128"
 #define MAX_DATA_LENGTH 256
@@ -72,15 +72,30 @@ int prevodniFaktorA = 18070;
 int prevodniFaktorB = 1000;
 
 static const char *TAG = "HTTP_SERVER";
+static const char *TAG2 = "TASK_MUJ_TASK";
+static const char *TAG3 = "TASK_MICHANI";
 static const char *TAG4 = "JSON_Parser";
-
 static const char *TAG5 = "TIME";
 static const char *TAG6 = "ulozDataZFormulare_handler";
 static const char *TAG7 = "prevodDatumu";
-//static const char *TAG8 = "tednevim";
+static const char *TAG8 = "Predvyplneni";
 static const char *TAG9 = "nactiPostrikData";
-// static const char *TAG5 = "TIME";
-
+static const char *TAG10 = "TARE";
+static const char *TAG11 = "zvazPripravek";
+static const char *TAG12 = "seradDatabaziPodleData";
+static const char *TAG13 = "smazaniPostriku_handler";
+static const char *TAG14 = "zmenaDatVDatabaziId_handler";
+static const char *TAG15 = "extract_id_from_json";
+static const char *TAG16 = "aktualizujDisplejMnozstvi";
+static const char *TAG17 = "aktualizujDenAplikace";
+static const char *TAG18 = "delete_postrik";
+static const char *TAG19 = "generate_id";
+static const char *TAG20 = "mamNecoKmichani";
+static const char *TAG21 = "pridatPostrik";
+static const char *TAG22 = "zpracujPostrikData";
+static const char *TAG23 = "nahlasPostriky_handler";
+static const char *TAG24 = "cekejNaFinalizaciMichani";
+static const char *TAG25 = "jePostrikVdatabazi";
 bool zacaloMichani = false;
 bool zahajenoPousteniVodyTlacitkem = false;
 bool kvitujiFinaleMichani = false;
@@ -100,24 +115,32 @@ void initialize_sntp(void);
 void obtain_time(void);
 static httpd_handle_t start_webserver(void);
 
+void aktualizujDenAplikace(int idStruktury, const char *denAplikace);
+void aktualizujDisplejMnozstvi(double potrebneMnozstviPripravku);
+void cekejNaFinalizaciMichani(char *osetrovanaPlodina);
+void cekejNaSpusteniVody();
 int delete_postrik(int id);
 bool extract_id_from_json(cJSON *json, int *id);
 int generate_id();
+bool jePostrikVdatabazi(PostrikData* postrik_data);
 bool lcd_update(const char *text, int line);
+bool mamNecoKmichani(void);
 void nactiPostrikData();
 void napustVodu(double litru);
 void parse_json_for_id(const char *json_str, PostrikData *data);
 void parse_json(const char *json_str, PostrikData *data);
+int porovnejDatumy(const char *datum1, const char *datum2);
 int porovnejPostrikData(PostrikData *a, PostrikData *b);
 void prevodDatumu(char *datum);
 void pridatPostrik(PostrikData novyPostrik);
 void print_current_date(char *buffer, size_t buffer_size);
 void print_current_time(char *buffer, size_t buffer_size);
 void seradDatabaziPodleData();
-void ulozPostrikData();
 void tare();
-
-
+void ulozPostrikData();
+void vypisStrukturuDoTerminalu();
+void zpracujPostrikData(int idStruktury, double *mnozstviPostriku, double *pomerMichani, char *nazevPripravku, char *osetrovanaPlodina);
+void zvazPripravek(double gramu);
 
 esp_err_t uvodniStranaKsicht_handler(httpd_req_t *req);
 esp_err_t nahlasPostriky_handler(httpd_req_t *req);
@@ -132,6 +155,7 @@ SemaphoreHandle_t Displej;
 SemaphoreHandle_t Tlacitko1;
 SemaphoreHandle_t Tlacitko2;
 SemaphoreHandle_t Tlacitko3;
+SemaphoreHandle_t mutexPostriku;
 
 PostrikData postrik_data = {.id = 0, .mnozstvi_postriku = 0.0, .pomer_michani = 0.0, .denPosledniAplikacePostriku = "02-02"};
 PostrikData dataBazePostriku[MAX_RUZNYCH_POSTRIKU];
